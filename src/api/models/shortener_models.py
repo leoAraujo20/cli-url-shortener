d580e6fa51e79e49deb913bb51f5f12e99588136
Leônidas Araújo <leonidasaraujomqm@gmail.com>
2026-05-28 11:14:54 -0300

    Create the new model URLAccess

--- src/api/models/shortener_models.py
+++ src/api/models/shortener_models.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from sqlmodel import Field, SQLModel
 
 from src.api.core.database import engine
@@ -6,6 +8,15 @@ from src.api.core.database import engine
 class URL(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     original_url: str
+
+
+class URLAccess(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    url_id: int = Field(foreign_key="url.id")
+    accessed_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    user_agent: str | None = None
+    referrer: str | None = None
+    ip_address: str | None = None
 
 
 SQLModel.metadata.create_all(engine)
